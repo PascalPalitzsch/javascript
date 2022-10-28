2d8d9678ae207aa4163a8420fbeb7fc48add93ae
pp031@hdm-stuttgart.de <pp031@hdm-stuttgart.de>
2022-10-28 08:28:41 +0200

    Some deleted files

--- 27_var_vs_let/index.js
+++
@@ -1,9 +0,0 @@
-// variable scope = where a variable is accessible
-
-// let = variables are lmited to block scope {}
-// var = variables are limited to a function(){}
-
-// global variable = is declared outside any function
-// (if gloabel, var will CHANGE brwoser´s window properties)
-
-let name = "Pascal";
