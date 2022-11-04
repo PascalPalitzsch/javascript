d985f08265129fae07d5746131ff6a57b59f1f6a
PascalPalitzsch <pascal.palitzsch@web.de>
2022-11-04 17:04:19 +0100

    javascript console time

---
+++ 66_console_time/index.js
@@ -0,0 +1,13 @@
+// console.time() = Starts a timer you can use to
+//                  track how long an operation takes
+//                  Give each timer a unique name.
+
+
+//start
+console.time("Response time");
+
+alert("Click the OK Button!");
+
+
+//end
+console.timeEnd("Response time");
