b2ef35181d59ef273af0958de06faff15bff3af8
PascalPalitzsch <pascal.palitzsch@web.de>
2022-11-07 11:12:44 +0100

    javascript promises

---
+++ 67_promises/index.js
@@ -0,0 +1,30 @@
+// promise = object that encapsulates the result of an asynchronous operation
+//           let asynchronous methods return values like synchronous methods
+//           "I promise to return something in the future"
+
+
+const promise = new Promise((resolve, reject) => {
+
+    let fileLoaded = false;
+
+    if(fileLoaded){
+        resolve("File loaded");
+    }
+    else{
+        reject("file NOT loaded");
+    }
+});
+
+
+promise.then(value => console.log(value))
+        .catch(error => console.log(error));
+
+
+
+
+
+const wait = time => new Promise(resolve => {
+    setTimeout(resolve, time);
+});
+
+wait(3000).then (() => console.log("Thanks for waiting!"));
