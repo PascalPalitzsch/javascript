3a048759afbfc2258de3448eec542a028ec36690
PascalPalitzsch <pascal.palitzsch@web.de>
2022-11-13 10:11:20 +0100

    javascript DOM intro

---
+++ 71_DOM_Intro/index.js
@@ -0,0 +1,13 @@
+// DOM = Document Object Model (API)
+//       An interface for changing the content of a page
+
+// console.dir(document);
+
+// console.log(document.title);
+// console.log(document.URL);
+
+// document.title = "Title goes here!";
+// document.location = "http://www.google.de";
+
+document.body.style.backgroundColor = "skyblue";
+document.getElementById("myDiv").innerHTML = "Hello";
