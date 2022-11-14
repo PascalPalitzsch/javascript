8ad269760b5bb5837ef17884ce2e3eb04a0f0cd1
PascalPalitzsch <pascal.palitzsch@web.de>
2022-11-14 10:11:47 +0100

    javascript DOM traversal

---
+++ 73_DOM_traversal/index.js
@@ -0,0 +1,11 @@
+let element = document.body;
+let child = element.firstElementChild;
+child.style.backgroundColor = "lightgreen";
+
+// .firstElementChild
+// .lastElementChild
+// .parentElement
+// .nextElementSibling
+// .previousElementSibling
+// .children[]
+// Array.from(.children)
