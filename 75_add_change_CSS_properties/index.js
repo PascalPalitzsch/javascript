d346db2d81d618788c67ec8588cfbda3b046f37c
PascalPalitzsch <pascal.palitzsch@web.de>
2022-11-14 10:13:46 +0100

    change CSS properties

---
+++ 75_add_change_CSS_properties/index.js
@@ -0,0 +1,9 @@
+const title = document.getElementById("myTitle");
+
+// title.style.backgroundColor = "rgb(50, 200, 250)";  // RGB
+title.style.backgroundColor = "#222222"; // HEX
+title.style.color = "rgb(50, 200, 250)";
+title.style.fontFamily = "consolas";
+title.style.textAlign = "center";
+title.style.border = "2px solid";
+title.style.display = "block";
